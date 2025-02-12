5ebf5accdafcaf70688e91130065b43afcf968c6
dee <178827694+dee797@users.noreply.github.com>
2025-02-12 16:47:40 -0700

    test: Add tests for testing constraints on Notification table

--- tests/db.test.js
+++ tests/db.test.js
@@ -1,33 +1,86 @@
-const userDB = require('../backend/db/userCRUD');
+const userTable = require('../backend/db/userCRUD');
+const notifTable = require('../backend/db/notificationCRUD');
 
 const today = new Date();
 
 /**
- *  a record for 'user' has been created in the test db
- *  to test constraints on various tables
+ *  To run the tests in this file, please first run 'node backend/db/populate-test-db.js
+ *  
+ *  This ensures that the necessary records are created in the test db in order to test
+ *  constraints on various tables
  * 
-    const user = {
+ *  These are two example records of users that will be populated in the db:
+
+    {
+        user_id: 1
         name: 'Kelly',
         handle: '@kelly',
         password: 'test',
         bio: '',
-        profilePicURL: '',
-        bannerPicURL: '',
-        dateJoined: new Date()
+        profile_pic_url: '',
+        banner_pic_url: '',
+        date_joined: new Date()
     }
-*/
 
-
-test('prevent user from being created if their chosen handle already exists', async () =>{
-    const user2 = {
-        name: 'Kelly2',
-        handle: '@kelly',
+    {
+        user_id: 2,
+        name: 'Kevin',
+        handle: '@kevin',
         password: 'test',
         bio: '',
-        profilePicURL: '',
-        bannerPicURL: '',
-        dateJoined: today
+        profile_pic_url: '',
+        banner_pic_url: '',
+        date_joined: new Date(),
     }
+
+ *  When finished testing you can run 'node backend/db/reset-test-db.js' to clear all
+ *  test tables of their records
+ * 
+ */
+
+describe('User table tests', () => {
+    test('prevent user from being created if their chosen handle already exists', async () =>{
+        const user2 = {
+            name: 'Kelly2',
+            handle: '@kelly',
+            password: 'test',
+            bio: '',
+            profile_pic_url: '',
+            banner_pic_url: '',
+            date_joined: today
+        }
+        
+        await expect(userTable.createUser(user2)).rejects.toThrow();
+    });
+});
+
+describe('Notification table tests', () => {
+    test('prevent notification from being created if receiver_id and sender_id are the same', async () => {
+        const notif = {
+            receiver_id: 1,
+            sender_id: 1,
+            source_url: 'test',
+            type_id: 1
+        }
     
-    await expect(userDB.createUser(user2)).rejects.toThrow();
-})
+        await expect(notifTable.createNotification(notif)).rejects.toThrow();
+    });
+
+    test('update notification read status to true', async () => {
+        const notif = {
+            notification_id: 1
+        }
+
+        await notifTable.updateNotification(notif);
+        await expect(notifTable.getNotification(notif)).resolves.toEqual({
+            notification_id: 1,
+            receiver_id: 2,
+            sender_id: 1,
+            source_url: 'test',
+            type_id: 1,
+            read_status: true
+        });
+    });
+});
+
+
