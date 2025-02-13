840a2d87f7e4d2ecb2b672a355946bde3f80cfbf
dee <178827694+dee797@users.noreply.github.com>
2025-02-13 12:38:17 -0700

    refactor: separate example users out into their own file so they can be used by different modules

---
+++ backend/db/exampleUsers.js
@@ -0,0 +1,23 @@
+const exampleUser1 = {
+    user_id: 1,
+    name: 'Kelly',
+    handle: '@kelly',
+    password: 'test',
+    bio: '',
+    profile_pic_url: '',
+    banner_pic_url: '',
+    date_joined: new Date('2025-01-01')
+}
+
+const exampleUser2 = {
+    user_id: 2,
+    name: 'Kevin',
+    handle: '@kevin',
+    password: 'test',
+    bio: '',
+    profile_pic_url: '',
+    banner_pic_url: '',
+    date_joined: new Date('2025-01-01'),
+}
+
+module.exports = { exampleUser1, exampleUser2 }
