d49b5fb8f4e8c8796a875d153761862b7a0e1cc7
dee <178827694+dee797@users.noreply.github.com>
2025-02-20 11:11:07 -0700

    refactor: getUserPosts func should include user's quote reposts and regular reposts, then sort them by date

--- backend/db/postCRUD.js
+++ backend/db/postCRUD.js
@@ -1,4 +1,6 @@
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require('@prisma/client');
+const { getReposts } = require('./repostCRUD');
+const { getQuoteReposts } = require('./quoteRepostCRUD');
 
 const prisma = new PrismaClient();
 
@@ -12,11 +14,46 @@ const createPost = async (post) => {
     });
 }
 
+// will get all posts, including reposts and quote reposts
+
 const getUserPosts = async (user) => {
-    const posts = await prisma.post.findMany({
-        where: { author_id: user.user_id }
+    const reposts = await getReposts({user_id: user.user_id});
+    const quoteReposts = await getQuoteReposts({user_id: user.user_id});
+
+    const posts = await prisma.$queryRaw`
+        select distinct p.* from "Post" p, "Reply", "Quote_Repost"
+        where p.post_id <> "Reply".reply_post_id
+        and p.post_id <> "Quote_Repost".quote_post_id
+        and p.post_id = ${user.user_id};
+    `;
+
+    const arr = Array.prototype.concat(reposts, quoteReposts, posts);
+
+    // sort records in arr by most recent date
+    arr.sort((a,b) => {
+        let aDate;
+        let bDate;
+
+        if ('quote_post' in a ) {
+            aDate = a.quote_post.date_created;
+        } else if ('parent_post' in a) {
+            aDate = a.parent_post.date_created;
+        } else {
+            aDate = a.date_created;
+        }
+
+        if ('quote_post' in b ) {
+            bDate = b.quote_post.date_created;
+        } else if ('parent_post' in b) {
+            bDate = b.parent_post.date_created;
+        } else {
+            bDate = b.date_created;
+        }
+
+        return new Date(aDate) - new Date(bDate);
     });
-    return posts;
+
+    return arr;
 }
 
 
