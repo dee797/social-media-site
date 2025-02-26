4017e238d80ad590e2a892d296dc3c3102804fa6
dee <178827694+dee797@users.noreply.github.com>
2025-02-26 15:55:59 -0700

    test: add tests for requests on /posts paths

--- backend/tests/userRoutes.test.js
+++ backend/tests/userRoutes.test.js
@@ -5,6 +5,7 @@ const userDB = require('../db/userCRUD');
 const {exampleUser1, exampleUser2} = require('../db/exampleUsers')
 
 let testJWT;
+
 /**
  *  To run the tests in this file, please first run 'node backend/db/populate-test-db.js
  *  
@@ -37,6 +38,7 @@ let testJWT;
  *  test tables of their records
  * 
  */
+
 
 describe("Setup tests (must be done before any other tests)", () => {
   // skip this test if you've already ran this test file once and the user "test" has been created
@@ -73,6 +75,24 @@ describe("Setup tests (must be done before any other tests)", () => {
         done();
       }
     });
+  });
+
+  test("update user info", done => {
+    userDB.getUserByHandle({handle: '@test'}).then((userTest) => {
+      request.agent(app)
+      .put(`/users/${userTest.user_id}`)
+      .auth(testJWT, {type: 'bearer'})
+      .type('form')
+      .send({
+        name: '123test',
+        profile_pic_url: 'abc',
+        banner_pic_url: 'abc',
+        bio: 'idk'
+      })
+      .expect("Content-Type", /json/)
+      .expect({updateSuccess: true})
+      .expect(200, done)
+      });
   });
 });
 
@@ -112,44 +132,94 @@ describe('GET tests for /users/:user_id path (these are all protected routes)', 
     .auth(testJWT, {type: 'bearer'})
     .expect("Content-Type", /json/)
     .expect({
-      userInfo: {...exampleUser1, date_joined: '2025-01-01T00:00:00.000Z'},
-      following: [{followed_user: {...exampleUser2, date_joined: '2025-01-01T00:00:00.000Z'}}],
-      followers: [],
-      likedPosts: [{ post: {
-        post_id: 2,
-        author: {user_id: 2, name: 'Kevin', handle: '@kevin', profile_pic_url: ''},
-        content: 'Hello World 2',
-        date_created: '2025-01-01T00:00:00.000Z',
-        numLikes: 1,
-        numReplies: 4,
-        numReposts: 1
-      }}]
-    })
+      "userInfo":{"user_id":1,"name":"Kelly","handle":"@kelly","bio":"","profile_pic_url":"","banner_pic_url":"","date_joined":"2025-01-01T00:00:00.000Z"},
+      "following":[{"followed_user":{"user_id":2,"name":"Kevin","handle":"@kevin","bio":"","profile_pic_url":"","banner_pic_url":"","date_joined":"2025-01-01T00:00:00.000Z"}}],
+      "likedPosts":[{
+        "post":{
+          "post_id":2,
+          "content":"Hello World 2",
+          "date_created":"2025-01-01T00:00:00.000Z",
+          "author":{"user_id":2,"name":"Kevin","handle":"@kevin","profile_pic_url":""},
+          "numLikes":1,"numReplies":4,"numReposts":1
+        }}],
+      "userPosts":{"name":"Kelly","username":"@kelly","profile_pic_url":"",
+        "posts":[
+          {
+          "parent_post":{"post_id":2,"content":"Hello World 2","date_created":"2025-01-01T00:00:00.000Z",
+            "author":{"user_id":2,"name":"Kevin","handle":"@kevin","profile_pic_url":""}},
+          "numLikes":1,"numReposts":1,"numReplies":4
+          },
+          {
+            "post_id":1,"author_id":1,"date_created":"2025-01-01T00:00:00.000Z","content":"Hello World",
+            "numLikes":0,"numReposts":1,"numReplies":0
+      }]},
+        "userReplies":{"name":"Kelly","username":"@kelly",
+          "replies":[
+            {
+              "reply_post":{"post_id":3,"author_id":1,"date_created":"2025-01-01T00:00:00.000Z","content":"I like this post"},
+              "parent_post":{"post_id":2,"author":{"name":"Kevin","handle":"@kevin","user_id":2}},
+              "numLikes":0,"numReposts":0,"numReplies":2
+            },
+            {
+              "reply_post":{"post_id":4,"author_id":1,"date_created":"2025-01-01T00:00:00.000Z","content":"I forgot to mention, this post rocks"},
+              "parent_post":{"post_id":2,"author":{"name":"Kevin","handle":"@kevin","user_id":2}},"numLikes":0,"numReposts":0,"numReplies":0
+            },
+            {
+              "reply_post":{"post_id":6,"author_id":1,"date_created":"2025-01-01T00:00:00.000Z","content":"No problem"},
+              "parent_post":{"post_id":5,"author":{"name":"Kevin","handle":"@kevin","user_id":2}},"numLikes":0,"numReposts":0,"numReplies":0
+            }
+          ]
+        }
+      }
+    )
     .expect(200, done);
   });
 });
 
 
-describe('POST/other tests for paths under /users', () => {
-  test("Update user info", done => {
+
+describe('GET tests for /users/:user_id/posts path (protected routes as well', () => {
+  test("posts prop should be empty array for @test, since they haven't created any posts", done => {
     userDB.getUserByHandle({handle: '@test'}).then((userTest) => {
       request.agent(app)
-      .put(`/users/${userTest.user_id}`)
+      .get(`/users/${userTest.user_id}/posts`)
       .auth(testJWT, {type: 'bearer'})
-      .type('form')
-      .send({
-        name: '123test',
-        username: '@kevin',
-        profile_pic_url: 'abc',
-        banner_pic_url: 'abc',
-        bio: 'idk'
-      })
       .expect("Content-Type", /json/)
-      .expect({updateSuccess: true})
-      .expect(200, done)
-      });
+      .expect({
+        name: "123test",
+        username: "@test",
+        profile_pic_url: "abc",
+        posts: []
+      })
+      .expect(200, done);
+    });
   });
 
+  test("replies prop should be empty array for @test, since they haven't created any replies", done => {
+    userDB.getUserByHandle({handle: '@test'}).then((userTest) => {
+      request.agent(app)
+      .get(`/users/${userTest.user_id}/posts/replies`)
+      .auth(testJWT, {type: 'bearer'})
+      .expect("Content-Type", /json/)
+      .expect({
+        name: "123test",
+        username: "@test",
+        replies: []
+      })
+      .expect(200, done);
+    });
+  });
+});
+
+
+
+describe.skip('POST/other tests for paths under /users', () => {
+  
+});
+
+
+
+describe('Security tests', () => {
   test("Successfully log out as @test user", done => {
     setTimeout(() => {
       request.agent(app)
@@ -159,6 +229,15 @@ describe('POST/other tests for paths under /users', () => {
       .expect({logoutSuccess: true})
       .expect(200, done)
       }, 1000);
+  });
+
+  test("Prevent reuse of token that was issued before @test logged out", done => {
+    request.agent(app)
+    .get("/users/1/profile")
+    .auth(testJWT, {type: 'bearer'})
+    .expect("Content-Type", /json/)
+    .expect({error: "An error has occurred."})
+    .expect(400, done);
   });
 
   test("send back errors for invalid inputs when creating new user", done => {
@@ -204,18 +283,6 @@ describe('POST/other tests for paths under /users', () => {
         }
       }
     })
-    .expect(400, done);
-  });
-});
-
-
-describe('Security tests', () => {
-  test("Prevent reuse of token that was issued before @test logged out", done => {
-    request.agent(app)
-    .get("/users/1/profile")
-    .auth(testJWT, {type: 'bearer'})
-    .expect("Content-Type", /json/)
-    .expect({error: "An error has occurred."})
     .expect(400, done);
   });
 })
