f055fed21b7ee755c157746ae0142f926891385d
dee <178827694+dee797@users.noreply.github.com>
2025-03-03 16:30:24 -0700

    Allow app to listen on PORT, allow cors from test origin

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,"public")));
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "http://localhost:5173",
   optionsSuccessStatus: 200
 }));
 
@@ -63,6 +63,6 @@ app.all("/*", (req, res, next) => {
 
 
 const PORT = process.env.PORT || 3000;
-//app.listen(PORT);
+app.listen(PORT);
 
 module.exports = app;
