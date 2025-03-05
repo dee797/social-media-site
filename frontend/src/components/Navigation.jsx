8be06a8b45cee05433f82ae714ef2effcd7bda73
dee <178827694+dee797@users.noreply.github.com>
2025-03-04 17:44:26 -0700

    build: create Navigation component that will act as a top-layer navbar for navigation

---
+++ frontend/src/components/Navigation.jsx
@@ -0,0 +1,75 @@
+import {useEffect, useState} from 'react';
+import { useLocation, useNavigate } from 'react-router';
+
+
+
+const fetchNotifications = (token, setCurrentUser, setNotifications, setError, setLoading, navigate) => {
+
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/home`, {
+        mode: "cors", 
+        method: "get",
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }})
+    .then(res => {
+        if (res.status > 401) {
+            throw new Error();
+        }
+        return res.json();
+    })
+    .then(res => {
+        if (res.error || !res.authenticated) {
+            setCurrentUser(null);
+            localStorage.clear();
+            return navigate("/login");
+        }
+
+        if (res.notifications) {
+            setNotifications(res.notifications);
+        }
+    })
+    .catch(err => {
+        setError(err);
+    })
+    .finally(() => {
+        setLoading(false);
+    });
+}
+
+
+
+const Navigation = ({ currentUser, setCurrentUser, token }) => {
+    const [notifications, setNotifications] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        if (token && currentUser) {
+            fetchNotifications(token, setCurrentUser, setNotifications, setError, setLoading, navigate);
+        } else {
+            setLoading(false);
+            navigate("/login");
+        }
+    }, [location]);
+
+
+    if (loading) return (<div className="h-screen w-screen flex items-center justify-center"><div className="loader mx-auto"></div></div>)
+
+    if (error) return (<p className="h-screen w-screen text-center">A network error was encountered.</p>);
+
+    return (
+        <>
+        <p>{notifications}</p>
+        </>
+    )
+}
+
+
+
+export { 
+    Navigation,
+    fetchNotifications
+}
