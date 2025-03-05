be1c82ceef5ad1de02b48ab634dc4ff93962d146
dee <178827694+dee797@users.noreply.github.com>
2025-03-04 17:42:41 -0700

    refactor: move fetch calls into their own separate functions for testing

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,85 @@
 import {useEffect, useState} from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useOutletContext } from "react-router-dom";
+
+
+const fetchLogin = (setCurrentUser, setAuthenticationError, setServerError, setLoading, formData, navigate) => {
+
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/users/login`, {
+        method: 'post',
+        mode: "cors",
+        body: JSON.stringify(formData),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(async res => {
+        try {
+            const resBody = await res.json();
+
+            if (res.status === 401 && resBody.message) {
+                return setAuthenticationError(resBody.message);
+            } else if (res.status > 401) {
+                throw new Error();
+            }
+
+            if (resBody.loginSuccess) {
+                localStorage.setItem('token', resBody.token);
+                localStorage.setItem('current_user_id', resBody.user.current_user_id);
+                setCurrentUser(resBody.user);
+                navigate("/");
+            }
+
+        } catch (err) {
+            throw new Error(err);
+        }
+    })
+    .catch(err => {
+        setServerError(err);
+    })
+    .finally(() => {
+        setLoading(false);
+    });
+}
+
+
+
+const checkUser = (token, setCurrentUser, setServerError, setLoading, navigate) => {
+
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/users/login`, {
+        method: "post",
+        mode: "cors",
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    })
+    .then(res => {
+        if (res.status > 401) {
+            throw new Error();
+        }
+        return res.json();
+    })
+    .then(res => {
+        if (res.error) {
+            localStorage.clear();
+            setCurrentUser(null);
+            return;
+        }
+
+        if (res.authenticated) {
+            navigate("/");
+        }
+    })
+    .catch(err => {
+        setServerError(err);
+    })
+    .finally(() => {
+        setLoading(false);
+    });
+}
+
+
 
 //setCurrentUser sets hook at App.jsx level
-const Login = ({setCurrentUser}) => {
-    const navigate = useNavigate();
+const Login = () => {
+    const [currentUser, setCurrentUser, token] = useOutletContext();
 
     const [formData, setFormData] = useState({
         username: "",
@@ -16,6 +92,7 @@ const Login = ({setCurrentUser}) => {
 
     const [loading, setLoading] = useState(true);
 
+    const navigate = useNavigate();
 
 
     const handleChange = event => {
@@ -27,87 +104,36 @@ const Login = ({setCurrentUser}) => {
     const handleSubmitLogin = event => {
         event.preventDefault();
         setLoading(true);
-
-        fetch(`${import.meta.env.VITE_BACKEND_URL}/users/login`, {
-            method: 'post',
-            body: JSON.stringify(formData),
-            headers: { 'Content-Type': 'application/json' }
-        })
-        .then(async res => {
-            try {
-                const resBody = await res.json();
-
-                if (res.status === 400 && resBody.message) {
-                    return setAuthenticationError(resBody.message);
-                } else if (res.status > 400) {
-                    throw new Error("Server error");
-                }
-
-                if (resBody.loginSuccess) {
-                    localStorage.setItem('token', res.token);
-                    setCurrentUser(res.current_user_id);
-                    navigate("/");
-                }
-
-            } catch (err) {
-                throw new Error(err);
-            }
-        })
-        .catch(err => {
-            setServerError(err);
-        })
-        .finally(() => {
-            setLoading(false);
-        });
+        
+        fetchLogin(setCurrentUser, setAuthenticationError, setServerError, setLoading, formData, navigate);
     }
 
 
     useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            fetch(`${import.meta.env.VITE_BACKEND_URL}/users/login`, {
-                method: "post",
-                headers: new Headers({
-                    "Authorization": `Bearer ${token}`
-                })
-            })
-            .then(res => {
-                if (res.status > 400) {
-                    throw new Error("Server error");
-                }
-                return res.json();
-            })
-            .then(res => {
-                if (res.authenticated) {
-                    navigate("/");
-                }
-            })
-            .catch(err => {
-                setServerError(err);
-            })
-            .finally(() => {
-                setLoading(false);
-            });
+        if (token && currentUser) {
+            checkUser(token, setCurrentUser, setServerError, setLoading, navigate);
         } else {
             setLoading(false);
         }
     }, []);
-    
+
+        
     if (serverError) return (<p className="h-screen w-screen text-center">A network error was encountered.</p>);
 
     return (
         <>
             <form onSubmit={handleSubmitLogin}>
-                {
-                    authenticationError
-                        ? <p>{authenticationError}</p>
-                        : null
-                }
+            
                 {   
                     loading 
                         ? <div className="h-screen w-screen flex items-center justify-center"><div className="loader mx-auto"></div></div>
-                        : 
+                        :
                         <>
+                        {
+                            authenticationError
+                                ? <p>{authenticationError}</p>
+                                : null
+                        }
                             <label>
                             Username
                             <input 
@@ -134,4 +160,4 @@ const Login = ({setCurrentUser}) => {
     )
 }
 
-export { Login }
+export { Login, checkUser, fetchLogin }
