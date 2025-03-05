9bee82c1e3802893ff882f4c69a1f0170899f6ab
dee <178827694+dee797@users.noreply.github.com>
2025-03-04 17:43:22 -0700

    build: create top level App component that stores currentUser state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import { Navigation } from './components/Navigation';
+import { Outlet, useNavigate } from 'react-router';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+// Use outlet context for currentUser info (id, profile pic, name, username, etc)
+// Render Navigation component within App since Navigation has no route of its own
+// also use state for location change-- whenever location changes then Navigation will rerender with new notifs
+const useFetchUserInfo = (token, currentUserID, setCurrentUser, setError, setLoading, navigate) => {
+
+  fetch(`${import.meta.env.VITE_BACKEND_URL}/users/${currentUserID}`, {
+      method: 'get',
+      mode: "cors",
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+  })
+  .then(res => {
+    if (res.status > 401) {
+      throw new Error();
+    }
+
+    return res.json();
+  })
+  .then(res => {
+    if (res.error || !res.authenticated) {
+      setCurrentUser(null);
+      localStorage.clear();
+      return navigate("/login");
+    }
+
+    if (res.userInfo) {
+      setCurrentUser(res.userInfo);
+    }
+  })
+  .catch(err => {
+    setError(err);
+  })
+  .finally(() => {
+    setLoading(false);
+  })
+}
+
+
+
+const App = () => {
+  const token = localStorage.getItem('token');
+  const currentUserID = localStorage.getItem('current_user_id');
+
+  const [currentUser, setCurrentUser] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token && currentUserID) {
+      useFetchUserInfo(token, currentUserID, setCurrentUser, setError, setLoading, navigate);
+    } else {
+      setLoading(false);
+      navigate("/login");
+    }
+  }, []);
+
+  if (loading) return (<div className="h-screen w-screen flex items-center justify-center"><div className="loader mx-auto"></div></div>)
+
+  if (error) return (<p className="h-screen w-screen text-center">A network error was encountered.</p>);
+
+  if (token && currentUser) return (
+    <>
+      <Navigation currentUser={currentUser} setCurrentUser={setCurrentUser} token={token}/>
+      <Outlet context={[currentUser, setCurrentUser, token]}/>
+    </>
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Outlet context={[currentUser, setCurrentUser, token]}/>
+  );
 }
 
 export default App
