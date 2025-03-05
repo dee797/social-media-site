e2f4d61396c5af2ac0b69a52924db5370b318e45
dee <178827694+dee797@users.noreply.github.com>
2025-03-04 17:44:50 -0700

    Update route structure

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -4,30 +4,23 @@ import { Login } from "./components/Login";
 
 
 const routes = [
-    /*
     {
         path: "/",
         element: <App />,
         errorElement: <ErrorPage />,
         children: [
+            { path: "/login", element: <Login /> },
+            /*
+            { path: "/signup", element: <Signup /> },
             { index: true, element: <Home /> },
-            // if authenticated and user tries to go to /signup, just redirect to home
-            { path: "/:handle", element: <Profile /> },
             { path: "/settings/profile", element: <Settings /> },
             { path: "/post/:id", element: <Post /> },
             { path: "/search", element: <Search /> },
+            { path: "/:handle", element: <Profile /> },
+        */
         ]
     },
-    {
-        path: "/signup",
-        element: <Signup />,
-        errorElement: <ErrorPage />
-    },*/
-    {
-        path: "/login", 
-        element: <Login />,
-        errorElement: <ErrorPage />
-    }
+    
 ];
 
 export default routes;
