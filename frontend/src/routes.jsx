f02696a036f1c040975433c0000191ed3d5ac1c0
dee <178827694+dee797@users.noreply.github.com>
2025-03-10 16:23:28 -0600

    Update imports and components for routes

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,6 +1,11 @@
 import App from "./App";
-import ErrorPage from "./components/ErrorPage";
-import { Login } from "./components/Login";
+import ErrorPage from "./pages/404ErrorPage";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
+import { Settings } from "./pages/Settings";
+import { NewPost } from "./pages/NewPost";
+import { ViewPost } from "./pages/ViewPost";
 
 
 const routes = [
@@ -9,18 +14,18 @@ const routes = [
         element: <App />,
         errorElement: <ErrorPage />,
         children: [
-            { path: "/login", element: <Login /> },
-            /*
-            { path: "/signup", element: <Signup /> },
             { index: true, element: <Home /> },
+            { path: "/login", element: <Login /> },
+            { path: "/signup", element: <Signup /> },
             { path: "/settings/profile", element: <Settings /> },
-            { path: "/post/:id", element: <Post /> },
+            { path: "/post", element: <NewPost />},
+            { path: "/post/:id", element: <ViewPost /> },
+            /*
             { path: "/search", element: <Search /> },
             { path: "/:handle", element: <Profile /> },
         */
         ]
-    },
-    
+    }   
 ];
 
 export default routes;
