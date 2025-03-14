0f513093a37173dbb23087196b4665b9e7fa3d23
dee <178827694+dee797@users.noreply.github.com>
2025-03-14 13:33:08 -0600

    build: wrap Spinner in div for positioning on page

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -2,7 +2,9 @@ import Spinner from 'react-bootstrap/Spinner';
 
 const Loader = () => {
     return (
-        <Spinner variant='primary'/>
+        <div className='d-sm-flex w-100 align-items-sm-center justify-content-sm-center' style={{marginTop: '30px'}}>
+            <Spinner variant='dark'/>
+        </div>
     )
 }
 
