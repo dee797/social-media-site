6ebbcbe520e72ba45b8d332016d93a3a05b6022b
dee <178827694+dee797@users.noreply.github.com>
2025-03-14 13:32:35 -0600

    build: use Link component instead of href for routing/navigating

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router';
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -11,19 +13,19 @@ const Navigation = ({ currentUser, setCurrentUser, token, setError }) => {
 
     return (
         <>
-            <Navbar expand="lg" id='navbar'>
+            <Navbar expand="lg" style={{backgroundColor: "lightskyblue", padding: "15px"}}>
                 <Container>
-                    <Navbar.Brand href="/">Home</Navbar.Brand>
-                    <Navbar.Toggle aria-controls='basic-navbar-nav'/>
-                    <Navbar.Collapse id='basic-navbar-nav'>
-                        <Nav className='me-auto'>
+                    <Navbar.Brand><Link to="/">Home</Link></Navbar.Brand>
+                    <SearchBar />
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                    <Navbar.Collapse id='basic-navbar-nav' style={{justifyContent:'flex-end'}}>
+                        <Nav style={{columnGap:'20px'}}>
                             <NavDropdown title="Notifications">
                                 <NotificationList token={token} currentUser={currentUser} setCurrentUser={setCurrentUser} setError={setError}/>
                             </NavDropdown>
-                            <SearchBar />
                             <NavDropdown title="User">
-                                <NavDropdown.Item href={`/${currentUser.handle}`}>View Profile</NavDropdown.Item>
-                                <NavDropdown.Item href="/settings/profile">Edit Profile</NavDropdown.Item>
+                                <Link to={`/${currentUser.handle.slice(1)}`}><NavDropdown.ItemText className='link'>View Profile</NavDropdown.ItemText></Link>
+                                <Link to="/settings/profile"><NavDropdown.ItemText className='link'>Edit Profile</NavDropdown.ItemText></Link>
                                 <NavDropdown.Item>Logout</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
