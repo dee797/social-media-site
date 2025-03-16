c3410bb858335432835956b3e3392d4acfa7b7d4
dee <178827694+dee797@users.noreply.github.com>
2025-03-15 18:07:33 -0600

    build: use Badge component to show user how many unread notifications they have

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,15 +1,19 @@
 import { Link } from 'react-router';
+import { useState } from 'react';
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Badge from 'react-bootstrap/Badge';
 
 import { SearchBar } from './SearchBar';
 import { NotificationList } from './NotificationList';
 
 
 const Navigation = ({ currentUser, setCurrentUser, token, setError }) => {
+    const [unreadNotifCount, setUnreadNotifCount] = useState(0);
+
 
     return (
         <>
@@ -30,11 +34,21 @@ const Navigation = ({ currentUser, setCurrentUser, token, setError }) => {
                         </Link>
                         
                         <NavDropdown id="notificationDropdown" title={
-                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" className="bi bi-bell-fill" viewBox="0 0 16 16">
-                            <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
-                            </svg>
+                                (
+                                    <>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" className="bi bi-bell-fill" viewBox="0 0 16 16">
+                                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
+                                        </svg>
+                                        {
+                                            unreadNotifCount > 0 ?
+                                            <Badge bg="light" style={{width: "30px", color: "var(--bs-secondary-color)", marginLeft: "10px"}}>{unreadNotifCount}</Badge> 
+                                            :
+                                            <div id="badgePlaceholder" style={{paddingLeft: "40px", display: "inline"}}></div>
+                                        }
+                                    </>
+                                )
                         }>
-                            <NotificationList token={token} currentUser={currentUser} setCurrentUser={setCurrentUser} setError={setError}/>
+                            <NotificationList token={token} currentUser={currentUser} setCurrentUser={setCurrentUser} setError={setError} setUnreadNotifCount={setUnreadNotifCount}/>
                         </NavDropdown>
                     </Navbar>
 
