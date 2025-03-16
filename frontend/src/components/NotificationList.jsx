0160ccf76f582e851222a1e89a0b7f762a1cb4c7
dee <178827694+dee797@users.noreply.github.com>
2025-03-15 18:09:05 -0600

    refactor: if notifications > 0, find all notifications that are unread and update unreadNotifCount  in parent component

--- frontend/src/components/NotificationList.jsx
+++ frontend/src/components/NotificationList.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
 import { useLocation, Navigate } from 'react-router';
-import { useFetchData } from '../helpers';
+import { useFetchData, handleSubmitForm, putData } from '../helpers';
 
 import { Notification } from './Notification';
+
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Spinner from 'react-bootstrap/esm/Spinner';
+import Button from 'react-bootstrap/Button';
+import Form from "react-bootstrap/Form";
 
 
-const NotificationList = ({token, currentUser, setCurrentUser, setError}) => {
+
+const NotificationList = ({token, currentUser, setCurrentUser, setError, setUnreadNotifCount}) => {
     const [notifications, setNotifications] = useState([]);
 
     const [loading, setLoading] = useState(true);
@@ -19,7 +23,6 @@ const NotificationList = ({token, currentUser, setCurrentUser, setError}) => {
     const expectedKey = 'notifications';
     useFetchData(token, currentUser, setCurrentUser, setNotifications, setError, setLoading, setNavigateTo, url, expectedKey, location);
 
-
     if (loading) return (
         <div className="d-sm-flex w-100 align-items-sm-center justify-content-sm-center" style={{height: '100px'}}>
             <Spinner size='sm'/>
@@ -28,19 +31,39 @@ const NotificationList = ({token, currentUser, setCurrentUser, setError}) => {
 
     if (navigateTo) return (<Navigate to={navigateTo}/>);
 
-    return ( 
-        <>
-            {
-                notifications.length === 0 
-                ? <NavDropdown.ItemText className='m-auto'>You currently have no notifications</NavDropdown.ItemText>
-                : notifications.map(notification => {
-                    return (
-                        <Notification notification={notification} key={notification.notification_id}/>
-                    );
-                })
+    if (notifications.length > 0) {
+        let unReadNotifCount = 0;
+        const notificationComponents = notifications.map(notification => {
+            if (!notification.read_status) {
+                unReadNotifCount += 1;
             }
-        </>
-    )  
+
+            return (<Notification notification={notification} key={notification.notification_id}/>);
+        })
+            
+        setUnreadNotifCount(unReadNotifCount);
+
+        return (
+            <>
+                <Form 
+                    method='post'
+                    onSubmit={(event) => {
+                        handleSubmitForm(event, setLoading, () => {
+                            const url = `${import.meta.env.VITE_BACKEND_URL}/users/${testCurrentUserId}/notifications`;
+                            putData(token, currentUser, setCurrentUser, setError, setLoading, setNavigateTo, url);
+                        });
+                    }}
+                >
+                    <Button type="submit">Mark all as read</Button>
+                </Form>
+                {notificationComponents}
+            </>
+        );
+    }
+
+    return (
+        <NavDropdown.ItemText className='m-auto'>You currently have no notifications</NavDropdown.ItemText>
+    );
 }
 
 export {
