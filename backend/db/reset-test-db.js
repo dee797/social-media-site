62b018eaf0cb38c2c09aaeca843be486a5960622
dee <178827694+dee797@users.noreply.github.com>
2025-04-03 12:51:46 -0600

    fix: don't delete records from Notification_Type table when resetting test data as notification types are not test data themselves

--- backend/db/reset-test-db.js
+++ backend/db/reset-test-db.js
@@ -6,15 +6,12 @@ async function main() {
     await prisma.follow.deleteMany({});
     await prisma.like.deleteMany({});
     await prisma.notification.deleteMany({});
-    await prisma.notification_Type.deleteMany({});
     await prisma.quote_Repost.deleteMany({});
     await prisma.reply.deleteMany({});
     await prisma.repost.deleteMany({});
     await prisma.post.deleteMany({});
     await prisma.user.deleteMany({});
 }
-
-
 
 
 module.exports = main;
