fbd070f07d7bb5b0889d3fc5f6a6390044738de4
dee <178827694+dee797@users.noreply.github.com>
2025-04-16 15:50:45 -0600

    Put frontend url in env variable to be used in cors

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,"public")));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
   optionsSuccessStatus: 200
 }));
 
